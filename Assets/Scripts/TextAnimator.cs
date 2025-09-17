a74dbf338c9adbb8d5e4a76f5e5c60eafef75ec2
RJGAMESHD <icecrafter721@gmail.com>
2025-09-17 13:52:18 +0800

    added hot reload to make things easier

--- Assets/Scripts/TextAnimator.cs
+++ Assets/Scripts/TextAnimator.cs
@@ -1,11 +1,18 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class TextAnimator : MonoBehaviour
 {
     private static GameObject text;
+
+    [SerializeField] private bool floating;
+    
+    [Header("Floating Animation Settings")]
     [Tooltip("Speed of the floating animation")]
+    [ShowIf("floating")]
     [SerializeField] private float speed = 2f;
     [Tooltip("How high the text floats up and down")]
+    [ShowIf("floating")]
     [SerializeField] private float amplitude = 0.5f;
 
     void Start()
